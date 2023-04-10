178a08e8be58583c399f8fdaaeec9ff43c71dc43
suraj gupta <surajgupta6477@gmail.com>
2023-04-10 21:46:18 +0530

    1stcommit

---
+++ library.js
@@ -0,0 +1,92 @@
+const missceleneous = [
+    {
+        id : 1,
+        date :"26 May, 2024",
+        name :"Temple 1 Comet",
+        description :"Tempel 1 (official designation: 9P/Tempel) is a periodic Jupiter-family comet discovered by Wilhelm Tempel in 1867. It completes an orbit of the Sun every 5.5 years. Tempel 1 was the target of the Deep Impact space mission, which photographed a deliberate high-speed impact upon the comet in 2005. ",
+        img_link : "https://en.wikipedia.org/wiki/Tempel_1",
+        more_info_link :"https://en.wikipedia.org/wiki/Tempel_1",
+
+    },
+    {
+        id : 2,
+        date :"7 August, 2027",
+        name :"	Asteroid (137108) 1999 AN10",
+        description :"(137108) 1999 AN10 is a kilometer-length near-Earth asteroid and potentially hazardous asteroid of the Apollo group. It was discovered by LINEAR on 13 January 1999.",
+        img_link :"https://en.wikipedia.org/wiki/(137108)_1999_AN10",
+        more_info_link :"https://en.wikipedia.org/wiki/(137108)_1999_AN10",
+
+    },
+    {
+        id : 3,
+        date : "26 October, 2028",
+        name :"Asteroid (35396) 1997 XF11",
+        description :"(35396) 1997 XF11, provisional designation 1997 XF11, is a kilometer-sized asteroid, classified as a near-Earth object, Mars-crosser and potentially hazardous asteroid of the Apollo group.",
+        img_link :"https://en.wikipedia.org/wiki/Asteroid",
+        more_info_link :"https://en.wikipedia.org/wiki/(35396)_1997_XF11",
+
+    },
+    {
+        id : 4,
+        date :"13 april,2029",
+        name :"Astroid (99942) Apophis",
+        description :"Near-Earth asteroid (99942) Apophis will pass Earth at a relatively small distance of 31,200 kilometres (19,400 mi) above Earth's surface, closer than some geosynchronous satellites.",
+        img_link :"https://en.wikipedia.org/wiki/Potentially_hazardous_object",
+        more_info_link :"https://en.wikipedia.org/wiki/99942_Apophis",
+
+    },
+    {
+        id : 5,
+        date :"17 March 2031",
+        name :"	Transit of Venus from Uranus.",
+        description :" Transit of Venus across the Sun takes place when the planet Venus passes directly between the Sun and a superior planet, becoming visible against (and hence obscuring a small portion of) the solar disk. During a transit, Venus can be seen from Earth as a small black dot moving across the face of the Sun.",
+        img_link :"https://en.wikipedia.org/wiki/Transit_of_Venus",
+        more_info_link :"https://en.wikipedia.org/wiki/Transit_of_Venus",
+
+    },
+    {
+        id : 6,
+        date :"29 October, 2031",
+        name :"Transit of Venus from Uranus",
+        description :"A transit of Venus across the Sun takes place when the planet Venus passes directly between the Sun and a superior planet, becoming visible against (and hence obscuring a small portion of) the solar disk.",
+        img_link :"https://en.wikipedia.org/wiki/Transit_of_Venus",
+        more_info_link :"https://en.wikipedia.org/wiki/Transit_of_Venus",
+
+    },
+    {
+        id : 7,
+        date :"17December, 2031",
+        name :"Transit of Earth from Uranus",
+        description :"Transit of Earth from Uranus.",
+        img_link : "https://en.wikipedia.org/wiki/Uranus",
+        more_info_link :"https://en.wikipedia.org/wiki/Uranus",
+
+    },
+    {
+        id :8,
+        date :"13 November, 2032",
+        name :"Transit of Mercury",
+        description :"A transit of Mercury across the Sun takes place when the planet Mercury passes directly between the Sun and a superior planet. During a transit, Mercury appears as a tiny black dot moving across the Sun as the planet obscures a small portion of the solar disk.",
+        img_link :"https://en.wikipedia.org/wiki/Transit_of_Mercury",
+        more_info_link :"https://en.wikipedia.org/wiki/Transit_of_Mercury",
+
+    },
+    {
+        id : 9,
+        date :"25 November, 2034",
+        name :"	Supermoon",
+        description :"A supermoon is a full moon or a new moon that nearly coincides with perigee—the closest that the Moon comes to the Earth in its elliptic orbit—resulting in a slightly larger-than-usual apparent size of the lunar disk as viewed from Earth.",
+        img_link :"https://en.wikipedia.org/wiki/Supermoon",
+        more_info_link :"https://en.wikipedia.org/wiki/Supermoon",
+
+    },
+    {
+        id : 10,
+        date :"2038",
+        name :"Comet 9P/Tempel 1",
+        description :"Comet 9P/Tempel 1 will pass near the Earth.Tempel 1 (official designation: 9P/Tempel) is a periodic Jupiter-family comet discovered by Wilhelm Tempel in 1867. It completes an orbit of the Sun every 5.5 years.",
+        img_link :"https://en.wikipedia.org/wiki/Tempel_1",
+        more_info_link :"https://en.wikipedia.org/wiki/Tempel_1",
+
+    }
+]
